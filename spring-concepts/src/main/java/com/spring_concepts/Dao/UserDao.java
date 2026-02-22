6ceff7fc9999969a3a41638c845301be4b1674a4
Manoj Kumar <mdnu066@gmail.com>
2026-02-22 17:04:06 +0530

    JDBC API

--- spring-concepts/src/main/java/com/spring_concepts/Dao/UserDao.java
+++ spring-concepts/src/main/java/com/spring_concepts/Dao/UserDao.java
@@ -14,14 +14,24 @@ import java.sql.Statement;
 public class UserDao {
     private final MySqlConnection connectionClass;
     
-    public void insertUser(UserDto user) throws SQLException {
-        Connection connection = connectionClass.getConnection();
-        Statement statement = connection.createStatement();
-        String insertQuery = "INSERT INTO Users (name, email, id) VALUES (?, ?, ?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getEmail());
-        preparedStatement.setInt(3, user.getId());
-        preparedStatement.executeUpdate();
+    public void insertUser(UserDto user) {
+        try (Connection connection = connectionClass.getConnection()){
+            Statement statement = connection.createStatement();
+            String insertQuery = "INSERT INTO Users (name, email, id) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setInt(3, user.getId());
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close(); // also needs to be closed
+            // if we don't close the connections, there will be the possibility of the the memory leacking also
+            // manual handling the DB connections pools
+        } catch (Exception ignored) {
+
+        } finally {
+
+        }
     }
 }
